24ed0d8470cb2f58b6388d2c78dffa05b114cea7
Daniel Shiloh <dshiloh@cscc.edu>
2026-07-25 12:59:31 -0400

    Bullets removed from list when off screen, added documentation

--- arsenal.py
+++ arsenal.py
@@ -15,26 +15,32 @@ if TYPE_CHECKING:
     
 
 class Arsenal:
+    """Create group to manage list of bullets"""
 
     def __init__(self, game: 'AlienInvasion'):
+        """create bullets as a group"""
         self.game = game
         self.settings = game.settings
         self.arsenal = pygame.sprite.Group()
 
     def update_arsenal(self):
+        """update list of bullet to track"""
         self.arsenal.update()
         self._remove_bullets_offscreen()
 
     def _remove_bullets_offscreen(self):
+        """remove bullets from right of screen"""
         for bullet in self.arsenal.copy():
-            if bullet.rect.bottom <= 0:
+            if bullet.rect.left >= self.settings.screen_w:
                 self.arsenal.remove(bullet)
 
     def draw(self):
+        """draw each bullet"""
         for bullet in self.arsenal:
             bullet.draw_bullet()
 
     def fire_bullet(self):
+        """is bullet available?  add if so"""
         if len(self.arsenal) < self.settings.bullet_amount:
             new_bullet = Bullet(self.game)
             self.arsenal.add(new_bullet)
