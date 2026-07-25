10ad90bea9daeffc3ee47125efb861fb8153b1bc
Daniel Shiloh <dshiloh@cscc.edu>
2026-07-25 12:49:48 -0400

    Moved HUD to right side of screen, added documentation

--- hud.py
+++ hud.py
@@ -14,8 +14,10 @@ if TYPE_CHECKING:
 
 
 class HUD:
+    """Create user display for game stats"""
 
     def __init__(self, game: 'AlienInvasion'):
+        """load font and stats, write lives, level, score to screen"""
         self.game = game
         self.settings = game.settings
         self.screen = game.screen
@@ -27,53 +29,62 @@ class HUD:
         self._setup_life_image()
         self.update_level()
 
-    def _setup_life_image(self):
-        self.life_image = pygame.image.load(self.settings.ship_file)
-        self.life_image = pygame.transform.scale(self.life_image,
-            (self.settings.ship_w, self.settings.ship_h)
-        )
-        self.life_rect = self.life_image.get_rect()
-
     def update_scores(self):
+        """update each score"""
         self._update_hi_score()
         self._update_max_score()
         self._update_score()
 
+    def _update_hi_score(self):
+        """show high score upper right"""
+        hi_score_str = f'Max-Score: {self.game_stats.hi_score: ,.0f}'
+        self.hi_score_image = self.font.render(hi_score_str, True, self.settings.text_color, None)
+        self.hi_score_rect = self.hi_score_image.get_rect()
+        self.hi_score_rect.right = self.boundaries.right - self.padding
+        self.hi_score_rect.top = self.padding
+
+    def _update_max_score(self):
+        """show max score (within game) under high score"""
+        max_score_str = f'Max-Score: {self.game_stats.max_score: ,.0f}'
+        self.max_score_image = self.font.render(max_score_str, True, self.settings.text_color, None)
+        self.max_score_rect = self.max_score_image.get_rect()
+        self.max_score_rect.right = self.boundaries.right - self.padding
+        self.max_score_rect.top = self.hi_score_rect.bottom + self.padding
+        
     def _update_score(self):
+        """show current score under max score"""
         score_str = f'Score: {self.game_stats.score: ,.0f}'
         self.score_image = self.font.render(score_str, True, self.settings.text_color, None)
         self.score_rect = self.score_image.get_rect()
         self.score_rect.right = self.boundaries.right - self.padding
         self.score_rect.top = self.max_score_rect.bottom + self.padding
 
-    def _update_max_score(self):
-        max_score_str = f'Max-Score: {self.game_stats.max_score: ,.0f}'
-        self.max_score_image = self.font.render(max_score_str, True, self.settings.text_color, None)
-        self.max_score_rect = self.max_score_image.get_rect()
-        self.max_score_rect.right = self.boundaries.right - self.padding
-        self.max_score_rect.top = self.padding
-
-    def _update_hi_score(self):
-        hi_score_str = f'Max-Score: {self.game_stats.hi_score: ,.0f}'
-        self.hi_score_image = self.font.render(hi_score_str, True, self.settings.text_color, None)
-        self.hi_score_rect = self.hi_score_image.get_rect()
-        self.hi_score_rect.midtop = (self.boundaries.centerx, self.padding)
-
     def update_level(self):
+        """show level in bottom right"""
         level_str = f'Level: {self.game_stats.level: ,.0f}'
         self.level_image = self.font.render(level_str, True, self.settings.text_color, None)
         self.level_rect = self.level_image.get_rect()
-        self.level_rect.left = self.padding
-        self.level_rect.top = self.life_rect.bottom + self.padding
+        self.level_rect.right = self.boundaries.right - self.padding
+        self.level_rect.bottom = self.boundaries.bottom - self.padding
+
+    def _setup_life_image(self):
+        """create smaller ship icon to display life count"""
+        self.life_image = pygame.image.load(self.settings.ship_file)
+        self.life_image = pygame.transform.scale(self.life_image,
+            (self.settings.ship_w/2, self.settings.ship_h/2)
+        )
+        self.life_rect = self.life_image.get_rect()
 
     def _draw_lives(self):
-        current_x = self.padding
-        current_y = self.padding
+        """show life count on top of level"""
+        current_x = self.boundaries.right - self.life_rect.width - self.padding
+        current_y = self.level_rect.top - self.life_rect.height - self.padding
         for _ in range(self.game_stats.ships_left):
             self.screen.blit(self.life_image, (current_x, current_y))
-            current_x += self.life_rect.width + self.padding
+            current_x -= (self.life_rect.width + self.padding)
 
     def draw(self):
+        """draw lives, level, scores, to screen"""
         self.screen.blit(self.hi_score_image, self.hi_score_rect)
         self.screen.blit(self.max_score_image, self.max_score_rect)
         self.screen.blit(self.score_image, self.score_rect)
