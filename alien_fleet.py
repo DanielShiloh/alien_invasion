d6fd576698a8aa4ad5eccbeede065fbaea9e62d8
Daniel Shiloh <dshiloh@cscc.edu>
2026-07-25 12:26:36 -0400

    Fleet dir changes based on hitting top/bottom of screen, added documentation

--- alien_fleet.py
+++ alien_fleet.py
@@ -16,6 +16,7 @@ if TYPE_CHECKING:
 class AlienFleet:
 
     def __init__(self, game: 'AlienInvasion'):
+        """set up and create fleet"""
         self.game = game
         self.settings = game.settings
         self.fleet = pygame.sprite.Group()
@@ -25,6 +26,7 @@ class AlienFleet:
         self.create_fleet()
 
     def create_fleet(self):
+        """manage planning and placing of fleet"""
         alien_w = self.settings.alien_w
         alien_h = self.settings.alien_h
         screen_w = self.settings.screen_w
@@ -36,24 +38,8 @@ class AlienFleet:
 
         self._create_rectangle_fleet(alien_w, alien_h, fleet_w, fleet_h, x_offset, y_offset)
 
-    def _create_rectangle_fleet(self, alien_w, alien_h, fleet_w, fleet_h, x_offset, y_offset):
-        for row in range(fleet_h):
-            for col in range(fleet_w):
-                current_x = alien_w * col + x_offset
-                current_y = alien_h * row + y_offset
-                if col % 2 == 0 or row % 2 == 0:
-                    continue
-                self._create_alien(current_x, current_y)
-
-    def calculate_offsets(self, alien_w, alien_h, screen_w, fleet_w, fleet_h):
-        half_screen = self.settings.screen_h//2
-        fleet_horizontal_space = fleet_w * alien_w
-        fleet_vertical_space = fleet_h * alien_h
-        x_offset = int((screen_w - fleet_horizontal_space)//2)
-        y_offset = int((half_screen - fleet_vertical_space)//2)
-        return x_offset,y_offset
-
     def calculate_fleet_size(self, alien_w, screen_w, alien_h, screen_h):
+        """from screen and alien sizes, find number of aliens to fit"""
         fleet_w = (screen_w//alien_w)
         fleet_h = ((screen_h / 2)//alien_h)
 
@@ -68,12 +54,34 @@ class AlienFleet:
             fleet_h -= 2
 
         return int(fleet_w), int(fleet_h)
+
+    def calculate_offsets(self, alien_w, alien_h, screen_w, fleet_w, fleet_h):
+        """from screen and alien sizes, find spacing at edges of screen"""
+        half_screen = self.settings.screen_h//2
+        fleet_horizontal_space = fleet_w * alien_w
+        fleet_vertical_space = fleet_h * alien_h
+        x_offset = int((screen_w - fleet_horizontal_space)//2)
+        y_offset = int((half_screen - fleet_vertical_space)//2)
+        return x_offset,y_offset
+    
+    def _create_rectangle_fleet(self, alien_w, alien_h, fleet_w, fleet_h, x_offset, y_offset):
+        """draw evenly-spaced aliens"""
+        for row in range(fleet_h):
+            for col in range(fleet_w):
+                current_x = alien_w * col + x_offset
+                current_y = alien_h * row + y_offset
+                if col % 2 == 0 or row % 2 == 0:
+                    continue
+                self._create_alien(current_x, current_y)
+
     
     def _create_alien(self, current_x: int, current_y: int):
+        """add alien to group of aliens"""
         new_alien = Alien(self, current_x, current_y)
         self.fleet.add(new_alien)
 
     def _check_fleet_edges(self):
+        """once an alien hits an edge, drop the whole fleet"""
         alien: Alien
         for alien in self.fleet:
             if alien.check_edges():
@@ -82,27 +90,33 @@ class AlienFleet:
                 break
 
     def _drop_alien_fleet(self):
+        """move aliens left, toward player"""
         for alien in self.fleet:
-            alien.y += self.fleet_drop_speed
+            alien.x -= self.fleet_drop_speed
 
     def update_fleet(self):
+        """check if at wall, then find new pos"""
         self._check_fleet_edges()
         self.fleet.update()
 
     def draw(self):
+        """draw each alien"""
         alien: 'Alien'
         for alien in self.fleet:
             alien.draw_alien()
 
     def check_collisions(self, other_group):
+        """rm alien and bullet if collision"""
         return pygame.sprite.groupcollide(self.fleet, other_group, True, True)
     
-    def check_fleet_bottom(self):
+    def check_fleet_left(self):
+        """has hit left wall?"""
         alien: Alien
         for alien in self.fleet:
-            if alien.rect.bottom >= self.settings.screen_h:
+            if alien.rect.left <= 0:
                 return True
         return False
     
     def check_destroyed_status(self):
+        """is fleet empty?"""
         return not self.fleet
